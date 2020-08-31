e96849411546f558686eb0dbdb8bb7214b8a912f
Marat <marathovsepyan45@gmail.com>
2020-09-01 00:39:40 +0400

    test

--- client/src/index.js
+++ client/src/index.js
@@ -11,10 +11,11 @@ import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import HomePageLoading from "./components/HomePageLoading/HomePageLoading";
 
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = createStore(rootReducer,compose(
      applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 ))
 const app =
     <Provider store={store}>
